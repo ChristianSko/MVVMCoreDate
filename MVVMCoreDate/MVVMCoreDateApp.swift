feb3c575210111075ccb09b1ae60bf45a88a44cd
Christian Skorobogatow <christiansko@icloud.com>
2022-07-06 11:22:22 +0200

    Initial Commit

---
+++ MVVMCoreDate/MVVMCoreDateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMCoreDateApp.swift
+//  MVVMCoreDate
+//
+//  Created by Christian Skorobogatow on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct MVVMCoreDateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
